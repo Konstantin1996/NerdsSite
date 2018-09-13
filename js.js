de1dab6b6d37b36e7639106a3f342d63d05b021b
Konstantin <kostyasupikos@gmail.com>
2018-09-14 01:14:57 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,7 @@
+var modalClose = document.getElementById('closer');
+console.log(modalClose);
+
+function closeWindow() {
+	var modalWindow = document.getElementById('window');
+	modalWindow.style.display = "none";
+}
